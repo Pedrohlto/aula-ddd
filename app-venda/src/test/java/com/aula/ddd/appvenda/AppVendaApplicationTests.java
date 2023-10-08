9dee53f52611643e1f450ed9b39e82c4d4576b0d
Pedro Oliveira <pedro_hlto@hotmail.com>
2023-10-08 18:32:25 -0300

    first commit

---
+++ app-venda/src/test/java/com/aula/ddd/appvenda/AppVendaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aula.ddd.appvenda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppVendaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
